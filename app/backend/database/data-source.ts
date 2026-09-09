667eac367735b2917c61f5a4c2803f244e818c86
Pierddev <139257364+Pierddev@users.noreply.github.com>
2026-09-09 17:06:36 +0200

    fix: ooooooohhhh commit fixes (#7)

--- app/backend/database/data-source.ts
+++ app/backend/database/data-source.ts
@@ -2,10 +2,6 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { join } from "node:path"
 import { env } from "../configs/env.ts"
-import { WordOfTheDay } from "../entities/wordOfTheDay.entity.ts"
-import { DayStats } from "../entities/dayStats.entity.ts"
-
-dotenv.config({ path: path.resolve(import.meta.dirname, "../../../.env") })
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -16,12 +12,5 @@ export const AppDataSource = new DataSource({
     database: env.dbName,
     entities: [join(import.meta.dirname, "../entities/**/*{.js,.ts}")],
     migrations: [join(import.meta.dirname, "migrations/**/*{.js,.ts}")],
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT ?? 5432),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [WordOfTheDay, DayStats],
-    migrations: [path.join(import.meta.dirname, "../migrations/**/*.{js,ts}")],
     synchronize: false,
 })
