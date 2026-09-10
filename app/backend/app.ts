d2d5b916253d084c80c59618c230fb3f11714b8d
Pierddev <139257364+Pierddev@users.noreply.github.com>
2026-09-10 09:52:19 +0200

    fix(express): server start fixes

--- app/backend/app.ts
+++ app/backend/app.ts
@@ -1,11 +1,11 @@
 import express, { type Express, type Request, type Response } from 'express';
 import { AppDataSource } from './database/data-source.ts';
-import { join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { readFileSync } from 'node:fs';
+import { env } from './configs/env.ts';
 
 import gamesRoutes from './routes/games.routes.ts';
 import statsRoutes from './routes/stats.routes.ts';
+
+const DEFAULT_PORT = 3000;
 
 export const createApp = (): Express => {
     const app: Express = express();
@@ -22,22 +22,24 @@ export const createApp = (): Express => {
     return app;
 };
 
-if (process.argv[1] === fileURLToPath(import.meta.url)) {
-	const pkg = JSON.parse(
-		readFileSync(join(import.meta.dirname, "package.json"), "utf-8"),
-	);
-	const port = Number(process.env.PORT) || 3000;
+// Code executend only when running "npm run dev"
+if (process.argv[1]?.endsWith('app.ts')) {
+	const port = Number(env.port) || DEFAULT_PORT;
 	const app = createApp();
 
-	AppDataSource.initialize()
-		.then(() => {
-			app.listen(port, () => {
-				console.log(
-					`Server ${pkg.name}@${pkg.version} (${process.env.NODE_ENV ?? "development"}) is running on http://localhost:${port}`,
-				);
-			});
-		})
-		.catch((err) => {
-			console.error("Error during Data Source initialization:", err);
+	try {
+		// DB connextion
+		await AppDataSource.initialize();
+		console.log('Connected to database');
+
+		// Express start
+		app.listen(port, () => {
+			console.log(`Server started at http://localhost:${port}`);
 		});
+	} catch (error) {
+		console.error("Can't connect to database.");
+		console.error('Tip : did you run "docker compose up -d" ?');
+		console.error(error);
+		process.exit(1);
+	}
 }
